a22484dc4a206ba4b3dbe2b2e33e98f20b544ba4
adeel <adeel.hassan5343@gmail.com>
2023-09-20 13:44:07 +0500

    adding file

--- akira/app/models.py
+++ akira/app/models.py
@@ -1,6 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+STATE_CHOICES = (
+    ('Bahawalpur','Bahawalpur'),
+    ('Faisalabad','Faisalabad'),
+    ('Gujranwala','Gujranwala'),
+    ('Lahore','Lahore'),
+    ('Mianwali','Mianwali'),
+    ('Multan','Multan'),
+    ('Rawalpindi','Rawalpindi'),
+    ('Sahiwal','Sahiwal'),
+    ('Sargodha','Sargodha'),
+)
 
 Category_choices=(
     ('SS','Sports Shirts'),
@@ -22,3 +34,19 @@ class Product(models.Model):
 
 def __str__(self):
         return self.title
+
+
+class Customer(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    mobile = models.IntegerField(default=0)
+    zipcode = models.IntegerField()
+    state = models.CharField(choices=STATE_CHOICES,max_length=100)
+    def __str__(self):
+        return self.name
+    
+
+
+        
--- akira/app/views.py
+++ akira/app/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.views import View
-from . models import Product
+from . models import Product,Customer
+from . forms import CustomerProfileForm, CustomerRegistrationForm
+from django.contrib import messages
+
+
 
 # Create your views here.
 def home(request):
     return render(request,'app/home.html')
+
+def contact(request):
+    return render(request,"app/contact.html")
 
 
 class CategoryView(View):
@@ -26,4 +33,67 @@ class ProductDetail(View):
     def get(self,request,pk):
         product = Product.objects.get(pk=pk)
         return render(request,"app/productdetail.html",locals())
+
+
+class CustomerRegistrationView(View):
+    def get(self,request):
+        form = CustomerRegistrationForm()
+        return render(request,"app/customerregistration.html",locals())
+
+    def post(self,request):
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"congratulations! User Register Successfully")
+        else:
+            messages.error(request,"Invalid Input Data")
+        return render(request,"app/customerregistration.html",locals())
+
+
+class ProfileView(View):
+    def get(self,request):
+        form = CustomerProfileForm()
+        return render(request,"app/profile.html",locals())
+    def post(self,request):
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid():
+            user = request.user
+            name = form.cleaned_data['name']
+            locality = form.cleaned_data['locality']
+            city = form.cleaned_data['city']
+            mobile = form.cleaned_data['mobile']
+            state = form.cleaned_data['state']
+            zipcode = form.cleaned_data['zipcode']
+
+            reg = Customer(user=user,name=name,locality=locality,mobile=mobile,city=city,state=state,zipcode=zipcode)
+            reg.save()
+            messages.success(request,"Congratulations! Profile Save Successfully")
+        else:
+            messages.warning(request,"Invalid Input Data")
+
+
+        return render(request,"app/profile.html",locals())
+
+
+def address(request):
+    add = Customer.objects.filter(user=request.user)
+    return render(request,'app/address.html',locals())
+
+
+
+class updateAddress(View):
+    def get(self,request,pk):
+        form = CustomerProfileForm()
+        return render(request,'app/updateAddress.html',locals())
+    def post(self,request,pk):
+        form = CustomerProfileForm(request.POST)
+        return render(request,'app/updateAddress.html',locals())
+
+
+
+
+        
+
+         
+
 
