15b945adeebfddce86ea80b67d12459c0953ba8e
adeel <adeel.hassan5343@gmail.com>
2023-09-21 14:01:03 +0500

    adding file

--- akira/app/forms.py
+++ akira/app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm,AuthenticationForm,UsernameField,PasswordChangeForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm,UsernameField,PasswordChangeForm, SetPasswordForm
 from django.contrib.auth.models import User
 
 from .models import Customer
@@ -29,8 +29,24 @@ class CustomerRegistrationForm(UserCreationForm):
         fields = ['username','email','password1','password2']
 
 
+class MyPasswordChangeForm(PasswordChangeForm):
+    old_password=forms.CharField(label='Old Password', widget= forms.PasswordInput(attrs={
+    'autofocus': 'True','autocomplete':'current-password','class': 'form-control'}))
+    new_password1 = forms.CharField(label='New Password', widget=forms.PasswordInput
+    (attrs={'autocomplete':'current-password','class': 'form-control'}))
+    new_password2 = forms.CharField(label='Conform Password', widget=forms.PasswordInput
+    (attrs={'autocomplete':'current-password','class': 'form-control'}))
+
+
 class MyPasswordResetForm(PasswordChangeForm):
     pass
+
+
+class MySetPasswordForm(SetPasswordForm):
+    new_password1 = forms.CharField(label='New Password', widget=forms.PasswordInput
+    (attrs={'autocomplete':'current-password','class': 'form-control'}))
+    new_password2 = forms.CharField(label='Conform New Password', widget=forms.PasswordInput
+    (attrs={'autocomplete':'current-password','class': 'form-control'}))
 
 class CustomerProfileForm(forms.ModelForm):
     class Meta:
--- akira/app/views.py
+++ akira/app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views import View
 from . models import Product,Customer
 from . forms import CustomerProfileForm, CustomerRegistrationForm
@@ -83,11 +83,25 @@ def address(request):
 
 class updateAddress(View):
     def get(self,request,pk):
-        form = CustomerProfileForm()
+        add = Customer.objects.get(pk=pk)
+        form = CustomerProfileForm(instance=add)
         return render(request,'app/updateAddress.html',locals())
     def post(self,request,pk):
         form = CustomerProfileForm(request.POST)
-        return render(request,'app/updateAddress.html',locals())
+        if form.is_valid():
+            add = Customer.objects.get(pk=pk)
+            add.name = form.cleaned_data['name']
+            add.locality = form.cleaned_data['locality']
+            add.city = form.cleaned_data['city']
+            add.mobile = form.cleaned_data['mobile']
+            add.state = form.cleaned_data['state']
+            add.zipcode = form.cleaned_data['zipcode']
+            add.save()
+            messages.success(request,"congratulations! Profile Update Successfully")
+        else:
+            messages.error(request,"Invalid Input Data")
+
+        return redirect("address")
 
 
 
