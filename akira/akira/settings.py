d12aa7d90fb327abb871fcdd403a5da7377bcf33
Iftikhar Ali <iftikharzulfiqar6@gmail.com>
2023-10-17 15:33:03 +0500

    Update Allow Host

--- akira/akira/settings.py
+++ akira/akira/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-@3kruxd_tyeho98tgn&npqwa0*#i&(p3!t@kbjshn-p6t@-q&q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*', '3.67.34.95']
 
 # Application definition
 
